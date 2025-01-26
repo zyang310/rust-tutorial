8d347f621b8c458475033b3914c0daec990f55ef
Zhi Hang Yang <zhiy@ad.unc.edu>
2025-01-26 19:32:56 +0000

    changed link in readme

---
+++ hello_rust/target/debug/deps/hello_rust-6a0cbc632c8b35dd.d
@@ -0,0 +1,5 @@
+/workspaces/rust-tutorial/hello_rust/target/debug/deps/libhello_rust-6a0cbc632c8b35dd.rmeta: src/main.rs
+
+/workspaces/rust-tutorial/hello_rust/target/debug/deps/hello_rust-6a0cbc632c8b35dd.d: src/main.rs
+
+src/main.rs:
---
+++ hello_rust/target/debug/deps/hello_rust-a30426c078929c0a.d
@@ -0,0 +1,5 @@
+/workspaces/rust-tutorial/hello_rust/target/debug/deps/libhello_rust-a30426c078929c0a.rmeta: src/main.rs
+
+/workspaces/rust-tutorial/hello_rust/target/debug/deps/hello_rust-a30426c078929c0a.d: src/main.rs
+
+src/main.rs:
